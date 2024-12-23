b68f5bc4148435cf76de875d8efe44c9f6fee1ac
joselinares17 <josedaniellinares135@gmail.com>
2024-12-23 14:56:14 -0500

    feat(user-service): adding token repository

---
+++ user-service/src/main/java/org/forumflow/backend/user/domain/repository/TokenRepository.java
@@ -0,0 +1,15 @@
+package org.forumflow.backend.user.domain.repository;
+
+import org.forumflow.backend.user.domain.entity.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TokenRepository extends JpaRepository<Token, Long> {
+    @Query("SELECT t FROM Token t INNER JOIN User u on t.user.id = u.id WHERE u.id = :id " +
+            "AND (t.expired = false OR t.revoked = false)")
+    List<Token> findAllValidTokenByUser(Long id);
+    Optional<Token> findByToken(String token);
+}
