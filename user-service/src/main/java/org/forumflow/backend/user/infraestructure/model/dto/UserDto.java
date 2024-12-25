a960db44d6caee2254c397ad104d90901e311924
joselinares17 <josedaniellinares135@gmail.com>
2024-12-25 16:38:57 -0500

    refactor(user-service): changing to use record

--- user-service/src/main/java/org/forumflow/backend/user/infraestructure/model/dto/UserDto.java
+++ user-service/src/main/java/org/forumflow/backend/user/infraestructure/model/dto/UserDto.java
@@ -1,18 +1,8 @@
 package org.forumflow.backend.user.infraestructure.model.dto;
 
-import lombok.Builder;
-import lombok.Data;
-
-@Data
-@Builder
-public class UserDto {
-    private Long id;
-    private String username;
-    private String password;
-
-    public UserDto(Long id, String username, String password) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-    }
+public record UserDto(
+        Long id,
+        String username,
+        String password
+) {
 }
